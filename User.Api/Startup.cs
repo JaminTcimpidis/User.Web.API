fc94c3303aa047ae573c851172c45f5ca4f7d3d0
JaminTcimpidis <Jamin.Tcimpidis@gmail.com>
2020-12-21 23:23:26 -0600

    removing commented out code

--- User.Api/Startup.cs
+++ User.Api/Startup.cs
@@ -21,10 +21,6 @@ namespace Users.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-                //.AddJsonOptions( options =>
-                //{
-                //    options.JsonSerializerOptions.Converters.Add(new UserJsonFormatter());
-                //});
             services.AddTransient<UserContext>(_ => new UserContext(Configuration.GetConnectionString("UserMysqlConnection")));
             services.AddCors(options => options.AddPolicy(_corsPolicy,
             builder =>
@@ -58,38 +54,4 @@ namespace Users.Api
             });
         }
     }
-
-
-    //class UserConverter : Newtonsoft.Json.JsonConverter
-    //{
-    //    public override bool CanConvert(Type objectType)
-    //    {
-    //        return (objectType == typeof(User));
-    //    }
-
-    //    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-    //    {
-    //        // Load the JSON for the Result into a JObject
-    //        JObject jo = JObject.Load(reader);
-
-    //        // Read the properties which will be used as constructor parameters
-    //        string firstName = (string)jo["firstName"];
-    //        string lastName = (string)jo["lastname"];
-    //        string email = (string)jo["email"];
-    //        // Construct the Result object using the non-default constructor
-    //        User result = new User(0, firstName, lastName, email);
-
-    //        // (If anything else needs to be populated on the result object, do that here)
-
-    //        // Return the result
-    //        return result;
-    //    }
-
-    //    public override bool CanWrite => false;
-
-    //    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //}
 }
